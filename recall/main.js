7376482db9cd94b687c910586a5e8972dc1097e1
zoldof <zoldof.game@gmail.com>
2026-05-05 17:56:31 +0900

    Update main.js

--- recall/main.js
+++ recall/main.js
@@ -1,10 +1,18 @@
+// -----------------------------------------------------------------
+// ① 定数・グローバル変数
+// -----------------------------------------------------------------
+
 const STORAGE_KEY = "shortcut_state";
 const general = "shortcut";
 const counts = 30;
 let shortcuts = [];
 let questions = [];
 let current = 0;
-  
+
+// -----------------------------------------------------------------
+// ② ユーティリティー
+// -----------------------------------------------------------------
+
 // CSV読込
 async function loadCSV(csvPath) {
   const res = await fetch(csvPath);
@@ -16,8 +24,101 @@ async function loadCSV(csvPath) {
   });
 }
 
+// フィッシャー・イェーツのシャッフル
+// デクリメントのデータの減少に合わせた効率的な並べ替えのアルゴリズム
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+// -----------------------------------------------------------------
+// ③ 状態保存・ロード
+// -----------------------------------------------------------------
+
+// 状態保存
+function saveState(state) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+}
+
+// 状態取得 or 初期化
+function loadState() {
+  const saved = localStorage.getItem(STORAGE_KEY);
+  let state = "";
+  if (saved) {
+    state = JSON.parse(saved)
+    if (state.index < state.order.length - counts) {
+      return state;
+    }
+  }
+
+  // 初回もしくは残存問題数が不足した場合
+  const shuffled = shuffle([...shortcuts]);
+
+  state = {
+    order: shuffled,
+    index: 0
+  };
+  
+  saveState(state);
+  return state;
+}
+
+// 今日の問題取得
+function getTodayQuestions() {
+  const state = loadState();
+  const start = state.index;
+  const end = Math.min(start + counts, state.order.length);
+  const today = state.order.slice(start, end);
+
+  // 次回のために更新
+  state.index = end;
+  saveState(state);
+
+  return today;
+}
+
+// -----------------------------------------------------------------
+// ④ 描画・実行設定
+// -----------------------------------------------------------------
+
+// 描画
+function render() {
+  document.getElementById("question").innerText = questions[current];
+  document.getElementById("progress").innerText = `${current + 1} / ${questions.length}`;
+}
+
+// 初期化
+function start() {
+  questions = getTodayQuestions();
+  current = 0;
+  render();
+}
+
+// 次へ
+function next() {
+  if (current < questions.length - 1) {
+    current++;
+    render();
+  }
+}
+
+// 戻る
+function prev() {
+  if (current > 0) {
+    current--;
+    render();
+  }
+}
+
+// -----------------------------------------------------------------
+// ⑤ DOM構築後のイベントハンドラ設定・初回実行
+// -----------------------------------------------------------------
+
 // 初期データ設定 
-// DOM が完成したらすぐ実行
+// DOM構築が完成したらすぐ実行
 document.addEventListener("DOMContentLoaded", async () => {
   // htmlで指定したdataset.csvからパスを取得し1列の配列に整形
   const appEl   = document.getElementById("app");
@@ -47,6 +148,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   const shctBtn = document.getElementById('shctBtn');
   const prevBtn = document.getElementById('prevBtn');
   const nextBtn = document.getElementById('nextBtn');
+
+  // イベントハンドラ設定
   answBtn.addEventListener('click', () => {
     newPath = csvUrl.pathname.replace(regex, "");
     externalUrl = `https://github.com/${user}/${repo}/blob/main/${newPath}`;
@@ -57,99 +160,10 @@ document.addEventListener("DOMContentLoaded", async () => {
     externalUrl = `${origin}/${newPath}/${general}/`;
     window.open(externalUrl, '_blank', 'noopener,noreferrer');
   });
-  prevBtn.addEventListener('click', () => {
-    if (current > 0) {
-      current--;
-      render();
-    }
-  });
-  nextBtn.addEventListener('click', () => {
-    if (current < questions.length - 1) {
-      current++;
-      render();
-    }
-  });
+  prevBtn.addEventListener('click', prev);
+  nextBtn.addEventListener('click', next);
   
+  // 初回実行
   start();
 });
 
-// フィッシャー・イェーツのシャッフル
-// デクリメントのデータの減少に合わせた効率的な並べ替えのアルゴリズム
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-// 状態取得 or 初期化
-function loadState() {
-  const saved = localStorage.getItem(STORAGE_KEY);
-  let state = "";
-  if (saved) {
-    state = JSON.parse(saved)
-    if (state.index < state.order.length - counts) {
-      return state;
-    }
-  }
-
-  // 初回もしくは残存問題数が不足した場合
-  const shuffled = shuffle([...shortcuts]);
-
-  state = {
-    order: shuffled,
-    index: 0
-  };
-  
-  saveState(state);
-  return state;
-}
-
-// 状態保存
-function saveState(state) {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
-}
-
-// 今日の問題取得
-function getTodayQuestions() {
-  const state = loadState();
-  const start = state.index;
-  const end = Math.min(start + counts, state.order.length);
-  const today = state.order.slice(start, end);
-
-  // 次回のために更新
-  state.index = end;
-  saveState(state);
-
-  return today;
-}
-
-// 初期化
-function start() {
-  questions = getTodayQuestions();
-  current = 0;
-  render();
-}
-
-// 描画
-function render() {
-  document.getElementById("question").innerText = questions[current];
-  document.getElementById("progress").innerText = `${current + 1} / ${questions.length}`;
-}
-
-// 次へ
-function next() {
-  if (current < questions.length - 1) {
-    current++;
-    render();
-  }
-}
-
-// 戻る
-function prev() {
-  if (current > 0) {
-    current--;
-    render();
-  }
-}
