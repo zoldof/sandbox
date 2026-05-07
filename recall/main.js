069ba12b3df07ce0a24f96589629e12b6edd9b6f
zoldof <zoldof.game@gmail.com>
2026-05-07 11:53:50 +0900

    Update main.js

--- recall/main.js
+++ recall/main.js
@@ -26,7 +26,7 @@ async function loadCSV(csvPath) {
 
 // フィッシャー・イェーツのシャッフル
 // デクリメントのデータの減少に合わせた効率的な並べ替えのアルゴリズム
-function shuffle(array) {
+export function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
