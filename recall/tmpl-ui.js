769b4bdf5e5ab28259cfb54d831fa77122ce018e
zoldof <zoldof.game@gmail.com>
2026-05-09 06:06:33 +0900

    Create tmpl-ui.js

---
+++ recall/tmpl-ui.js
@@ -0,0 +1,4 @@
+const src = await fetch('/sandbox/recall/ui.html').then(r => r.text());
+const tmpl = document.getElementById('uiTemplate');
+tmpl.innerHTML = src.trim();
+document.body.appendChild(tmpl.content.cloneNode(true));
