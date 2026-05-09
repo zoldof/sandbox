bd253969da0de66b57bf645c75280387a2805acd
zoldof <zoldof.game@gmail.com>
2026-05-10 04:52:03 +0900

    Update select.js

--- recall/select.js
+++ recall/select.js
@@ -1,6 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
   const navs = document.getElementById('navSelect');
   if (navs) {
     navs.addEventListener('change', function () {
       if (this.value) location.href = this.value;
     });
   }
+});
