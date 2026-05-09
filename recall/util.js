e099a949531209b4b43a634869413d0565063c53
zoldof <zoldof.game@gmail.com>
2026-05-09 17:24:39 +0900

    Update util.js

--- recall/util.js
+++ recall/util.js
@@ -1,11 +1,11 @@
 //  同じ文字を使わずに len 文字のランダム文字列を作る
-export const CHAR_POOL = [
+const CHAR_POOL = [
   ...'ABCDEFGHIJKLMNOPQRSTUVWXYZ',   // 大文字
   ...'0123456789',                     // 数字
   ...'!@#$%^&*()-_=+[]{};:,<.>/?'      // 記号（好きなもので増減可）
 ];
 
-export function randomString(len) {
+function randomString(len) {
   const pool = [...CHAR_POOL];
   let result = '';
   for (let i = 0; i < len; i++) {
@@ -13,6 +13,17 @@ export function randomString(len) {
     result += pool.splice(idx, 1)[0]; // 使った文字はプールから除去
   }
   return result;
+}
+
+export function generateProblems(cnt, len) {
+  const problems = [];
+  for (let i = 0; i < cnt; i++) {
+    problems.push({
+      id: i,
+      text: randomString(len)   // 正解文字列
+    });
+  }
+  return problems;
 }
 
 // フィッシャー・イェーツのシャッフル
