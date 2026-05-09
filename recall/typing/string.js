8f06cf75febb501717f1fdc278bd5e2477a7443c
zoldof <zoldof.game@gmail.com>
2026-05-09 18:14:34 +0900

    Update string.js

--- recall/typing/string.js
+++ recall/typing/string.js
@@ -3,7 +3,7 @@ import { createNavigator } from "/sandbox/recall/nav.js";
 
 const length = 8;
 const count = 10;
-const problems = await generateProblems(count, length);   // ページ読み込み時に一度だけ作成
+const problems = generateProblems(count, length);   // ページ読み込み時に一度だけ作成
 
 // ---------- 描画関数 ----------
 function renderStr(idx) {
