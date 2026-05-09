a703df51bbdb4b875ca8fcd12ad64240c14d7e04
zoldof <zoldof.game@gmail.com>
2026-05-10 05:19:37 +0900

    Update call-tmpl_ui.js

--- recall/call-tmpl_ui.js
+++ recall/call-tmpl_ui.js
@@ -2,4 +2,5 @@ const src = await fetch('/sandbox/recall/ui.html').then(r => r.text());
 const tmpl = document.getElementById('uiTemplate');
 tmpl.innerHTML = src.trim();
 document.body.appendChild(tmpl.content.cloneNode(true));
+debug("1");
 
