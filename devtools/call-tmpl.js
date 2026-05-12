cf4079cb3b6706063e2bb589e55984219682f86d
zoldof <zoldof.game@gmail.com>
2026-05-13 07:11:04 +0900

    Update call-tmpl.js

--- devtools/call-tmpl.js
+++ devtools/call-tmpl.js
@@ -1,4 +1,4 @@
 const src = await fetch('/sandbox/devtools/eruda.html').then(r => r.text());
-const tmpl = document.getElementById('uiTemplate');
+const tmpl = document.getElementById('erudaTemplate');
 tmpl.innerHTML = src.trim();
 document.body.appendChild(tmpl.content.cloneNode(true));
