f8c2fda3c5d984e6a2cfc790e4f40ed072a3b0ab
zoldof <zoldof.game@gmail.com>
2026-05-14 07:30:02 +0900

    Update eruda.js

--- devtools/eruda.js
+++ devtools/eruda.js
@@ -2,5 +2,5 @@
 // 本番で無効化したい場合は、環境変数やクエリパラメータで制御してください。
 // 例: if (location.search.includes('debug')) { eruda.init(); }
 eruda.init();               // トグルボタン生成
-//eruda.add(erudaDom());      // DOM 検査プラグイン（任意）
-eruda.add(erudaFps());      // FPS 表示プラグイン（任意）
+eruda.add(erudaDom());      // DOM 検査プラグイン（任意）
+//eruda.add(erudaFps());      // FPS 表示プラグイン（任意）
