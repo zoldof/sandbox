1bd4a67c4434bbf710e288f20b751fa1633c5bc7
zoldof <zoldof.game@gmail.com>
2026-05-15 18:56:05 +0900

    Update slide.js

--- recall/a-process/slide.js
+++ recall/a-process/slide.js
@@ -51,12 +51,12 @@ nav.bindButtons(
 
 // 平均所要時間を計測する
 avgEl = document.getElementById('avgTime');
-startTime = showAverage(questions, startTime, timerId, avgEl);
+startTime = showAverage(shuffledSlides, startTime, timerId, avgEl);
 timerId = setTimeout(() => { avgEl.textContent = "計測上限超過"; }, MAX_TIME_MS);
 
 mesrBtn = document.getElementById('avgTime');
 mesrBtn.addEventListener('click', () => {
-  startTime = showAverage(questions, startTime, timerId, avgEl);
+  startTime = showAverage(shuffledSlides, startTime, timerId, avgEl);
   mesrBtn.disabled = true;
 });
 
