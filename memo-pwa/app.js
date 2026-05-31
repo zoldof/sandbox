4660729d97becb6720057e545b849b06c6ac86e7
zoldof <zoldof.game@gmail.com>
2026-05-31 18:12:17 +0900

    Create app.js

---
+++ memo-pwa/app.js
@@ -0,0 +1,139 @@
+let fileHandle = null;
+
+const editor =
+    document.getElementById("editor");
+
+document
+    .getElementById("newBtn")
+    .addEventListener("click", newFile);
+
+document
+    .getElementById("openBtn")
+    .addEventListener("click", openFile);
+
+document
+    .getElementById("saveBtn")
+    .addEventListener("click", saveFile);
+
+document
+    .getElementById("saveAsBtn")
+    .addEventListener("click", saveAsFile);
+
+document
+    .getElementById("searchInput")
+    .addEventListener("input", searchText);
+
+async function newFile() {
+
+    editor.value = "";
+    fileHandle = null;
+}
+
+async function openFile() {
+
+    try {
+
+        const [handle] =
+            await window.showOpenFilePicker({
+                types: [{
+                    description: "Text Files",
+                    accept: {
+                        "text/plain": [".txt"]
+                    }
+                }]
+            });
+
+        fileHandle = handle;
+
+        const file =
+            await handle.getFile();
+
+        editor.value =
+            await file.text();
+
+    } catch (error) {
+
+        console.error(error);
+    }
+}
+
+async function saveFile() {
+
+    try {
+
+        if (!fileHandle) {
+
+            await saveAsFile();
+            return;
+        }
+
+        const writable =
+            await fileHandle.createWritable();
+
+        await writable.write(
+            editor.value
+        );
+
+        await writable.close();
+
+    } catch (error) {
+
+        console.error(error);
+    }
+}
+
+async function saveAsFile() {
+
+    try {
+
+        fileHandle =
+            await window.showSaveFilePicker({
+
+                suggestedName: "memo.txt",
+
+                types: [{
+                    description: "Text Files",
+                    accept: {
+                        "text/plain": [".txt"]
+                    }
+                }]
+            });
+
+        await saveFile();
+
+    } catch (error) {
+
+        console.error(error);
+    }
+}
+
+function searchText(event) {
+
+    const keyword =
+        event.target.value;
+
+    if (!keyword) {
+        return;
+    }
+
+    const pos =
+        editor.value.indexOf(keyword);
+
+    if (pos === -1) {
+        return;
+    }
+
+    editor.focus();
+
+    editor.setSelectionRange(
+        pos,
+        pos + keyword.length
+    );
+}
+
+if ("serviceWorker" in navigator) {
+
+    navigator.serviceWorker
+        .register("./service-worker.js")
+        .catch(console.error);
+}
