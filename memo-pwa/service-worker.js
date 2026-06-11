a3ed2cc251fca07db3d487b3ce1c84dcd87c3f8d
zoldof <zoldof.game@gmail.com>
2026-06-12 06:46:32 +0900

    Update service-worker.js

--- memo-pwa/service-worker.js
+++ memo-pwa/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "memo-pwa-v1";
+const CACHE_NAME = "memo-pwa-v2";
 
 const FILES = [
     "./",
@@ -10,13 +10,36 @@ const FILES = [
 
 self.addEventListener("install", event => {
 
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES))
+    );
+});
+
+self.addEventListener("activate", event => {
+
     event.waitUntil(
 
-        caches.open(CACHE_NAME)
-            .then(cache =>
-                cache.addAll(FILES)
+        Promise.all([
+
+            self.clients.claim(),
+
+            caches.keys().then(keys =>
+
+                Promise.all(
+                    keys
+                        .filter(key => key !== CACHE_NAME)
+                        .map(key => caches.delete(key))
+                )
+
             )
+
+        ])
+
     );
+
 });
 
 self.addEventListener("fetch", event => {
@@ -24,8 +47,26 @@ self.addEventListener("fetch", event => {
     event.respondWith(
 
         caches.match(event.request)
-            .then(response =>
-                response || fetch(event.request)
-            )
+            .then(cachedResponse => {
+
+                const fetchPromise = fetch(event.request)
+                    .then(networkResponse => {
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(
+                                    event.request,
+                                    networkResponse.clone()
+                                );
+                            });
+
+                        return networkResponse;
+                    });
+
+                return cachedResponse || fetchPromise;
+
+            })
+
     );
+
 });
