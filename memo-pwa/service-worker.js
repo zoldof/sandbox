b79dfb5ea531d8ef86d4a1e6b083c37635433b67
zoldof <zoldof.game@gmail.com>
2026-06-21 18:33:44 +0900

    Update service-worker.js

--- memo-pwa/service-worker.js
+++ memo-pwa/service-worker.js
@@ -34,19 +34,9 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
-            .then(cachedResponse => {
-                const fetchPromise = fetch(event.request)
-                    .then(networkResponse => {
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(
-                                    event.request,
-                                    networkResponse.clone()
-                                );
-                            });
-                        return networkResponse;
-                    });
-                return cachedResponse || fetchPromise;
-            })
+        .then(cached => {
+            if(cached) return cached;
+            return fetch(event.request);
+        })
     );
 });
