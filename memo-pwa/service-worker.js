7e2826cc041e2368970bc8cb208b15325cb6b579
zoldof <zoldof.game@gmail.com>
2026-06-21 18:47:44 +0900

    Update service-worker.js

--- memo-pwa/service-worker.js
+++ memo-pwa/service-worker.js
@@ -32,11 +32,16 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
+     if(event.request.mode === "navigate"){
+        event.respondWith(
+            caches.match("./index.html")
+        );
+        return;
+    }
     event.respondWith(
         caches.match(event.request)
-        .then(cached => {
-            if(cached) return cached;
-            return fetch(event.request);
+        .then(response => {
+            return response || fetch(event.request);
         })
     );
 });
